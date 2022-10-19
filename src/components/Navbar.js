fe9b43aaec16cdaefd92b2dda37520580d9be05d
eneskalkann <eneskalkan36e@gmail.com>
2022-10-20 00:07:18 +0300

    add-nav-link
    for router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import "../Design/designNavbar/Navbar.css"
 import image from '../Assets/Logo.svg'
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from 'react';
+import { NavLink } from "react-router-dom";
 
 
 function Navbar() {
@@ -25,10 +26,10 @@ function Navbar() {
       <img src={image} alt="unifeed" />
       <nav>
         <ul ref={navbarRef}>
-          <li className='home-item' style={{navActiveStyle}}><a>Home</a></li>
-          <li style={{navActiveStyle}}><a>Features</a></li>
-          <li style={{navActiveStyle}}><a>Pricing</a></li>
-          <li style={{navActiveStyle}} className='subscribe'><a>Subscribe</a></li>
+          <NavLink exact to="/" className='items home-item' style={{navActiveStyle}}><a>Home</a></NavLink>
+          <NavLink to="" className='items' style={{navActiveStyle}}><a>Features</a></NavLink>
+          <NavLink to="" className='items' style={{navActiveStyle}}><a>Pricing</a></NavLink>
+          <NavLink to="/subs" className='items subscribe' style={{navActiveStyle}}><a>Subscribe</a></NavLink>
           <button onClick={showNavbar} className="navbar-btn navbar-btn-close">
             <FaTimes/>
           </button>
