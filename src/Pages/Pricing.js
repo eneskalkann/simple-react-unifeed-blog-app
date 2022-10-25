4143d5630981e31b2898545640c57e9afa0b278e
eneskalkann <eneskalkan36e@gmail.com>
2022-10-25 23:24:41 +0300

    add-header

--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import PricingMain from '../components/PricingMain'
+import styled from 'styled-components'
 
 function Pricing() {
     return (
         <div className='pricingContainer'>
             <Navbar/>
+            <br /><br /><br /><br />
+            <Header>Our Pricing</Header>
             <PricingMain/>
             <br /><br /><br /><br />
             <Footer/>
@@ -14,5 +17,15 @@ function Pricing() {
     )
 }
 
+const Header = styled.h1`
+    display: flex;
+    justify-content: center;
+    margin-top: 0px;
+    margin-bottom: 40px;    
+    color: rgb(91, 94, 93);
+    @media screen and (max-width:620px){
+        margin-bottom: -50px;
+    }
+`
 
 export default Pricing
